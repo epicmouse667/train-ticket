b6d715704ee26d42e0b64b1135bc88d469eeccd2
epicmouse <2544547787@qq.com>
2022-07-01 15:48:17 +0800

    fix all bugs

--- ts-travel-service/src/main/java/travel/TravelApplication.java
+++ ts-travel-service/src/main/java/travel/TravelApplication.java
@@ -20,7 +20,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAsync
 @IntegrationComponentScan
 @EnableSwagger2
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class TravelApplication {
 
     public static void main(String[] args) {
